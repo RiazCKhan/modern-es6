fbf54b17295d95e606bbe0c7ea88e230e66b261f
RiazK <RiazCKhan@gmail.com>
2022-04-19 15:43:23 -0400

    ex2 complete

--- module-1/default-arguements/exercises.js
+++ module-1/default-arguements/exercises.js
@@ -13,3 +13,20 @@ function executeCallback( callback, delay = 1000 ) {
 
 //Edit above this line 
 executeCallback( () => console.log('Done'));
+
+/* 
+Change the below code such that the second argument of printComment 
+has a default value that’s initially 1, and is incremented by 1 after each call.
+
+Select the show console button in the widget below to see your output.
+*/
+
+let count = 1
+function printComment( comment, line = count++) {
+  console.log( line, comment );
+}
+
+
+//Edit above this line
+for (var i = 1; i <= 5; i++)
+printComment('I should be lineNumber ' + i);

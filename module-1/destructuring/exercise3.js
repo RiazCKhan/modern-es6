73b3ea3dda5e45c7149777fc77adb9bce687ee47
RiazK <RiazCKhan@gmail.com>
2022-05-30 14:08:25 -0400

    inital commit

---
+++ module-1/destructuring/exercise3.js
@@ -0,0 +1,7 @@
+// Create one destructuring expression that declares exactly one variable
+// to retrieve x.A[2]. Return the value in a new variable called A_2.
+
+let x = { A: [ 't', 'e', 's', 't' ] };
+
+//Write your Code here
+let A_2 = ""; 

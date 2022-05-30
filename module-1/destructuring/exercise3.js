5eaf505f4748a7aad36ce7438c4495c6cbbf203e
RiazK <RiazCKhan@gmail.com>
2022-05-30 14:15:03 -0400

    solved

--- module-1/destructuring/exercise3.js
+++ module-1/destructuring/exercise3.js
@@ -1,7 +1,11 @@
 // Create one destructuring expression that declares exactly one variable
 // to retrieve x.A[2]. Return the value in a new variable called A_2.
 
-let x = { A: [ 't', 'e', 's', 't' ] };
+let x = {
+  A: ['t', 'e', 's', 't']
+};
 
 //Write your Code here
-let A_2 = ""; 
+let { A : [ , , A_2 ] } = x;
+
+console.log(A_2)
